3201c41604dd6cc7914288c3489ff277fecc1528
Nikola Radan <nikola.radan@Y22-Design-Nikola-Radan2.local>
2018-09-17 15:24:27 -0400

    SlopeCalculator update

--- SlopeCalculator.py
+++ SlopeCalculator.py
@@ -1,1 +1,20 @@
 print("Slope Calculator:")
+
+#input
+x1 = input("input x1: ")
+x1 = int(x1)
+
+y1 = input("input y1: ")
+y1 = int(y1)
+
+x2 = input("input x2: ")
+x2 = int(x2)
+
+y2 = input("input y2: ")
+y2 = int(y2)
+
+#process
+rise = x2 - x1
+run = y2 - y1
+
+#output
